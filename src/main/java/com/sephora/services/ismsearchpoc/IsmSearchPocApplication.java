85a6f1ba17c3bb3ba2eaa64388f98c10c8df0211
Ram Subramanian (ramsub) <ramsub@icloud.com>
2025-08-15 12:43:08 -0700

    Initial Commit

---
+++ src/main/java/com/sephora/services/ismsearchpoc/IsmSearchPocApplication.java
@@ -0,0 +1,13 @@
+package com.sephora.services.ismsearchpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmSearchPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsmSearchPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sephora/services/ismsearchpoc/IsmSearchPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sephora.services.ismsearchpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmSearchPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
